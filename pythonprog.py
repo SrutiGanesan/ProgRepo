a8febd7ead3ada75e8f0441edd825096ba2f4f35
SrutiGanesan <70736678+SrutiGanesan@users.noreply.github.com>
2021-05-07 11:36:55 +0530

    Add files via upload

---
+++ pythonprog.py
@@ -0,0 +1,6 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=2
+y=3
+sum=x+y
+print("sum")
